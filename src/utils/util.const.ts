de1ab3167fc3c5eb2176aad082778d7fe225d449
Abel Ani <abelanico6@gmail.com>
2024-09-11 23:35:16 +0100

    Update

--- src/utils/util.const.ts
+++ src/utils/util.const.ts
@@ -1,4 +1,5 @@
-export const BASE_URL = 'http://localhost:8080';
+// export const BASE_URL = 'http://localhost:8080';
+export const BASE_URL = 'http://162.254.32.201:8080';
 
 export const NAIRA_SIGN = '₦';
 
